0410fdcad26bd77527dba80a5779a7a25d00d3d0
UncleVirus <101526319+UncleVirus@users.noreply.github.com>
2023-01-04 00:02:36 +0300

    Add files via upload

---
+++ Temp Conversion/temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+fixed_cost_for_setting_up_the_board_manufacturing_machine = 100.0
+
+variable_cost_for_producing_each_board_on_the_machine = 2.0
+
+quantity_of_boards_produced = 100000
+
+total_cost_for_producing_boards = fixed_cost_for_setting_up_the_board_manufacturing_machine + quantity_of_boards_produced * variable_cost_for_producing_each_board_on_the_machine
